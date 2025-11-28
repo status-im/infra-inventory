b6d2aab90f9875cc2c68757746ab95d44f7148ce
apentori <pentori.alexis@proton.me>
2025-11-28 17:28:54 +0100

    feat: adding keycloak authentication
    Signed-off-by: apentori <pentori.alexis@proton.me>

--- infra_inventory/config.py
+++ infra_inventory/config.py
@@ -10,10 +10,15 @@ class Config:
     CONSUL_PORT = os.getenv('CONSUL_PORT', '8500')
     CONSUL_SCHEME = os.getenv('CONSUL_SCHEME', 'http')
     CONSUL_TOKEN = os.getenv('CONSUL_TOKEN', '')
-    CONSUL_DC = os.getenv('CONSUL_DC', 'dc1')
 
     # Refresh configuration
     REFRESH_INTERVAL = int(os.getenv('REFRESH_INTERVAL', '3600'))  # 1 hour in seconds
     PORT = int(os.getenv('PORT', 5000))
     DEBUG = os.getenv('DEBUG', False)
     LOG_LEVEL = os.getenv('LOG_LEVEL', 'INFO')
+    SECRET_KEY = os.getenv('SECRET_KEY')
+    # Oauth
+    OAUTH_CLIENT_ID=os.getenv('OAUTH_CLIENT_ID', 'inventory')
+    OAUTH_CLIENT_SECRET=os.getenv('OAUTH_CLIENT_SECRET')
+    OAUTH_ISSUER=os.getenv('OAUTH_ISSUER')
+    OAUTH_SCOPE=os.getenv('OAUTH_SCOPE','openid profile email roles')
